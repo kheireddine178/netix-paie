18d92cf36e93355f45051babca9636132d6bab51
Kheireddine Kharrouby <kheireddine.kharrouby.pro@gmail.com>
2026-07-05 18:30:22 +0100

    Delete app/app/parametres/parametres directory

--- app/app/parametres/parametres/page.tsx
+++
@@ -1,9 +0,0 @@
-import { getParametresComplets } from "./actions";
-import ParametresForm from "./ParametresForm";
-
-export const dynamic = "force-dynamic";
-
-export default async function ParametresPage() {
-  const parametres = await getParametresComplets();
-  return <ParametresForm initial={parametres} />;
-}
